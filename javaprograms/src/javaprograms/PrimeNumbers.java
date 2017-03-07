47e75199a1dbd82ca0fe1f7695d89168788d436d
Vamshi Edulakanti <vedulakanti@callone.com>
2017-03-06 21:50:30 -0600

    my first commit

---
+++ javaprograms/src/javaprograms/PrimeNumbers.java
@@ -0,0 +1,24 @@
+package javaprograms;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter the Number:");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n%2==0) {
+			System.out.println("number is even");
+			
+		}
+		if(n%2!=0){
+			System.out.println("number is not a odd");
+			System.out.println("helloworld");
+		}
+
+	}
+
+}
