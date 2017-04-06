58a8a7aa3e3536a355b74fc9122a12bba956122e
Vamshi Edulakanti <vedulakanti@callone.com>
2017-04-06 16:37:40 -0500

    syso added

--- javaprograms/src/javaprograms/EvenNumber.java
+++ javaprograms/src/javaprograms/EvenNumber.java
@@ -1,10 +1,12 @@
 package javaprograms;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 public class EvenNumber {
 	
 	private void syso() {
 		// TODO Auto-generated method stub
-
+syso("this is vamshi ");
 	}
 
 }
